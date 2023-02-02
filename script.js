db1ca66937859e7a4c46009d214ee0253de95f90
Kako mekin <kako.mekinulovi@gmail.com>
2023-02-02 13:24:54 +0200

    js rgb and chosen circle added

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let rgbCode = document.getElementById('rgbCode')
 
-function start() {
-    
+function rgb() {
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+    return 'rgb('+r+','+g+','+b+')'
+
 }
+rgbCode.innerHTML = rgb() //rgb(r,g,b)
+function applycolor(button , rgb) {
+    button.setAttribute('style', `background: ${rgb}`)
+
+}
+let randomlyChosen = document.getElementById(Math.floor(Math.random() * 6)+1)
+applycolor(randomlyChosen, rgb())
+document.getElementsByTagName("button")
+
